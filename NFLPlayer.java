aff4f3ffd45567f0dcf09fb7188a58766da47674
JenniferDavis <jdavis190@my.gcu.edu>
2016-09-25 01:39:14 -0400

    Topic 4 Milestone Assignment  4: Create Getters and Setters

--- NFLPlayer.java
+++ NFLPlayer.java
@@ -1,56 +1,44 @@
 import java.util.*;
 /*
  * Author: Jennifer Davis
- * CST-100 Assignment Topic 2
- * Last Edited: September 11, 2016
+ * CST-100 Assignment Topic 4
+ * Last Edited: September 24, 2016
  * 
  * This program allows a coach to draft available players for an NFL team.
+ * Players include position and stats.
+ * User can add and remove players from the Chosen Players List.
+ * Final list is displayed and program exits.
  */
 
 
 public class NFLPlayer {
 
 	public static void main(String[] args) {
-		
+		// Create PlayerManager object
 		PlayerManager manage = new PlayerManager();
+		
+		// Opening Screen to start game
+		System.out.println("------NFL DRAFT BOARD------");
+		System.out.println("Pick Your Team");
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter \"Y\" to start:");
+		String start = input.next();
+		System.out.println();
+		if(start.equalsIgnoreCase("Y"))
+		{
+			PlayerManager.startGame();
+		}
+		else
+		{
+			System.exit(0);
+		}
 	
+		/*
 		System.out.println("There are " + manage.Players.length + " players to choose from.");
 		System.out.println("\nList of Players:\n");
 		Arrays.stream(manage.Players).forEach(System.out::println);
+		*/
 	}
-	
-	//Players 
-	String Player1 = "Drew Brees";
-	String Player2 = "Kwon Alexander";
-	String Player3 = "Lamar Miller";
-	String Player4 = "Leonard Williams";
-	String Player5 = "A.J. Green";
-	String Player6 = "Brandon Carr";
-	String Player7 = "Alex Smith";
-	String Player8 = "Amari Cooper";
-	String Player9 = "Andy Dalton";
-	String Player10 = "Danny Woodhead";
-
-	
-	//Player stats
-	int passingYards = 0;
-	int tackles = 0;
-	int rushingYards = 0;
-	double sacks = 0;
-	int receivingYards = 0;
-	int interceptions = 0;
-	
-
-	//Player positions
-	String position1 = "QB";
-	String position2 = "RB";
-	String position3 = "WR";
-	String position4 = "TE";
-	String position5 = "OL";
-	String position6 = "DL";
-	String position7 = "LB";
-	String position8 = "DB";
-	String position9 = "K";
-	String position10 = "P";
 	
 }
--- PlayerManager.java
+++ PlayerManager.java
@@ -1,7 +1,10 @@
+import java.util.*;
+
 /*
  * Author: Jennifer Davis
  * CST-100 Assignment Topic 3
- * Last Edited: September 17, 2016
+ * CST-100 Assignment Topic 4
+ * Last Edited: September 24, 2016
  * 
  * This class manages the players used in NFLPlayer.java
  */
@@ -9,31 +12,223 @@
 
 public class PlayerManager {
 	
-	public PlayerManager(){
+	public static String [] players = new String[10];
+	public static String [] positions = new String[10];
+	public static double [] statistics = new double[6];
+	
+	//Players 
+	public static String player1 = "1. Drew Brees";
+	public static String player2 = "2. Kwon Alexander";
+	public static String player3 = "3. Lamar Miller";
+	public static String player4 = "4. Leonard Williams";
+	public static String player5 = "5. A.J. Green";
+	public static String player6 = "6. Brandon Carr";
+	public static String player7 = "7. Alex Smith";
+	public static String player8 = "8. Amari Cooper";
+	public static String player9 = "9. Andy Dalton";
+	public static String player10 = "10. Danny Woodhead";
+	
+	//Player positions
+	public static String position1 = "QB";
+	public static String position2 = "RB";
+	public static String position3 = "WR";
+	public static String position4 = "TE";
+	public static String position5 = "OL";
+	public static String position6 = "DL";
+	public static String position7 = "LB";
+	public static String position8 = "DB";
+	public static String position9 = "K";
+	public static String position10 = "P";
+	
+	//Player stats
+	public static double passingYards = 0.0;
+	public static double tackles = 0;
+	public static double rushingYards = 0;
+	public static double sacks = 0;
+	public static double receivingYards = 0;
+	public static double interceptions = 0;
+	
+	public PlayerManager()
+	{
 		createPlayers();
+		createPositions();
+		statistics();
+	}
+	
+	// instructions, display Available Players List, and request user input
+	public static void startGame()
+	{
+		displayInstructions();
+		availablePlayersList();
+		getUserInput();
+	}
+
+	// display instructions 
+	public static void displayInstructions()
+	{
+		System.out.println("INSTRUCTIONS:\nTo add an Available Player to the Chosen List, enter the player's number.\n" 
+				+ "Enter \"C\" to view the Chosen List.  \nEnter \"A\" to see the Available List. "
+				+ "\nEnter \"F\" to finalize the draft list.");
+		System.out.println();
+	}
+	
+	// display Available Players List
+	public static void availablePlayersList()
+	{
+		System.out.println("AVAILABLE PLAYERS LIST:");
+		Arrays.stream(players).forEach(System.out::println); // will have to change array to available array
+		System.out.println();
+	}
+	
+	// display Chosen Players List
+	public static void chosenPlayersList()
+	{
+		System.out.println("CHOSEN PLAYERS LIST:");
+		Arrays.stream(players).forEach(System.out::println); // will have to change array to available array
+		System.out.println();
+	}
+		
+	// display Finalized List
+	public static void displayFinalizedList()
+	{
+		System.out.println("FINALIZED PLAYER DRAFT LIST:");
+		Arrays.stream(players).forEach(System.out::println); // will have to change array to available array
+		System.out.println();
+	}
+	
+	// get user input
+	public static void getUserInput()
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a player number, \"A\", \"C\", or \"F\": ");
+		String usersChoice = input.next();
+		if (usersChoice == "A")
+		{
+			// display the Available Players List
+		}
+		else if (usersChoice == "C")
+		{
+			// display the Chosen Players List
+		}
+		else if (usersChoice == "F")
+		{
+			// display the finalized list
+		}
+		else 
+		{
+			// either add or remove (switch) the player depending on which list user is on
+		}	
+	}
+	
+	// put players into Players array
+	public static void createPlayers(){
+		players[0] = player1;
+		players[1] = player2;
+		players[2] = player3;
+		players[3] = player4;
+		players[4] = player5;
+		players[5] = player6;
+		players[6] = player7;
+		players[7] = player8;
+		players[8] = player9;
+		players[9] = player10;
+	}
+	public static void createPositions(){
+		positions[0] = position1;
+		positions[1] = position2;
+		positions[2] = position3;
+		positions[3] = position4;
+		positions[4] = position5;
+		positions[5] = position6;
+		positions[6] = position7;
+		positions[7] = position8;
+		positions[8] = position9;
+		positions[9] = position10;
+	}
+	
+	public static void statistics(){
+		statistics[0] = passingYards;
+		statistics[1] = tackles;
+		statistics[2] = rushingYards;
+		statistics[3] = sacks;
+		statistics[4] = receivingYards;
+		statistics[5] = interceptions;
 	}
 	
 
-	// create object from NFLPlayer main class so that I can access the variables already set there.
-	NFLPlayer nflPlayer = new NFLPlayer(); 
-	
-	// create an array to hold the list of available players
-	public static String[] Players = new String[10];
-	
-	//create a method called CreatePlayers to put players into Players array
-	public void createPlayers(){
-		Players[0] = "Drew Brees";
-		Players[1] = nflPlayer.Player2;
-		Players[2] = nflPlayer.Player3;
-		Players[3] = nflPlayer.Player4;
-		Players[4] = nflPlayer.Player5;
-		Players[5] = nflPlayer.Player6;
-		Players[6] = nflPlayer.Player7;
-		Players[7] = nflPlayer.Player8;
-		Players[8] = nflPlayer.Player9;
-		Players[9] = nflPlayer.Player10;
+	public String getPlayer(int i)
+	{
+	    return players[i];
 	}
-		
+	
+	public void setPlayer(int i, String value)
+	{
+	     players[i] = value;
+	}
+	public String getPosition(int i)
+	{
+	    return positions[i];
+	}
+	
+	public void setPosition(int i, String value)
+	{
+	     positions[i] = value;
+	}
+	public double getPassingYards(double i)
+	{
+	    return statistics[(int)i];
+	}
+	
+	public void setPassingYards(double i, double value)
+	{
+		statistics[(int)i] = value;
+	}
+	public double getTackles(double i)
+	{
+	    return statistics[(int)i];
+	}
+	
+	public void setTackles(double i, double value)
+	{
+		statistics[(int)i] = value;
+	}
+	public double getRushingYards(double i)
+	{
+	    return statistics[(int)i];
+	}
+	
+	public void setRushingYards(double i, double value)
+	{
+		statistics[(int)i] = value;
+	}
+	public double getSacks(double i)
+	{
+	    return statistics[(int)i];
+	}
+	
+	public void setSacks(double i, double value)
+	{
+		statistics[(int)i] = value;
+	}
+	public double getReceivingYards(double i)
+	{
+	    return statistics[(int)i];
+	}
+	
+	public void setReceivingYards(double i, double value)
+	{
+		statistics[(int)i] = value;
+	}
+	public double getInterceptions(double i)
+	{
+	    return statistics[(int)i];
+	}
+	
+	public void setInterceptions(double i, double value)
+	{
+		statistics[(int)i] = value;
+	}	
+	
 }
 
 
